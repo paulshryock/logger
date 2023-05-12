c744a912c41432a37b60e4b21c1b22b2a7df2a1a
Paul Shryock <paul@pshry.com>
2023-05-12 14:00:31 -0400

    Add PSR-3 interfaces

---
+++ src/Psr/Log/LogLevel.ts
@@ -0,0 +1,13 @@
+/**
+ * Describes log levels.
+ */
+export enum LogLevel {
+  EMERGENCY = 'emergency',
+  ALERT     = 'alert',
+  CRITICAL  = 'critical',
+  ERROR     = 'error',
+  WARNING   = 'warning',
+  NOTICE    = 'notice',
+  INFO      = 'info',
+  DEBUG     = 'debug',
+}
