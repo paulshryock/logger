6088fa7b313b711d11f001573fb59ac69beb5be8
Paul Shryock <paul@pshry.com>
2023-05-12 21:02:12 -0400

    Add FileSystemLogger class (wip)

---
+++ tests/Logger/FileSystemLogger/FileSystemLogger.test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from '@jest/globals'
+
+describe('FileSystemLogger', () => {
+  it.todo('should instantiate')
+  it.todo('should throw if the level is invalid')
+  it.todo('should log a message to the file system')
+  it.todo('should should not overwrite an existing file')
+  it.todo('should append messages to an existing file')
+  it.todo('should allow different levels to log to different files')
+})
